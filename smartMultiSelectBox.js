0aa6d9d92bc35656a26829426b6a0ea674be6855
Chinmay <chinmay.bijwe@gmail.com>
2011-12-20 20:36:18 -0500

    Problem with css

--- smartMultiSelectBox.js
+++ smartMultiSelectBox.js
@@ -1,7 +1,34 @@
 (function($){
-  $.fn.smsBox = function (options) {
+  $.fn.smsBox = function (method, options) {
     var settings = $.extend ({
       'location' : 'top' 
-    }, options)
-  }
+    }, options),
+
+    methods = {
+    init : function( options ) { 
+      // THIS 
+    },
+    show : function( ) {
+      // IS
+    },
+    hide : function( ) { 
+      // GOOD
+    },
+    update : function( content ) { 
+      // !!! 
+    }
+
+  };
+  if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+    }    
+
+    return this.each (function() {
+      
+    });
+  };
 })(jQuery);
