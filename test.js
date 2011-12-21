1bedabc30229db20bbeb7c6e9c761d8294548d35
Chinmay <chinmay.bijwe@gmail.com>
2011-12-21 01:37:07 -0500

    slight css improvements and more data

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 $('document').ready(function(){
-  $("#example").smsBox({'data': {1:"Sudheer",2:"Chinmay",3:"Awesome smsBox"}});
+  $("#example").smsBox({'data': {1:"Sudheer",2:"Chinmay",3:"Awesome smsBox", 4:"getting ready", 5:"readier"}});
 });
